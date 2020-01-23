46253d833710f19c240a3df92d814c4946a97c6a
Oskari Timgren <oskari.timgren@gmail.com>
2020-01-22 19:23:04 -0500

    Added requeing to batch script

--- state-propagation/main.py
+++ state-propagation/main.py
@@ -117,7 +117,8 @@ def generate_batchfile(options_dict):
     with open(batchfile_path, 'r') as f:
         lines = f.readlines()
     
-    text = '#SBATCH --partition '+options_dict["cluster_params"]["partition"]+'\n' 
+    text = ('#SBATCH --partition '+options_dict["cluster_params"]["partition"]
+            +'\n#SBATCH --requeue')
     lines.insert(1, text)
     
     with open(batchfile_path, 'w') as f:
